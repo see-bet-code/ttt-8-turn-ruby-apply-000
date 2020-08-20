2bd9d800ad143d413909356b610c39058b49e376
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-20 14:24:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,17 @@ end
 def valid_move?(board, index)
   !position_taken?(board, index) && index.between?(0,8)
 end
+
+def move
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move(board, index)
+    move
+  else
+    turn(board)
+  end
+end
